4ce64feca8f745b0ec1343cb59269a1788c6d081
Vladimir Osipovich <osipovich.vova@gmail.com>
2023-02-25 15:07:17 +0300

    fix bugs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: false,
-  // images: { unoptimized: true },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
-      __nextDefaultLocale: { page: '/index' },
-      // '/': { page: '/index' },
+      '/': { page: '/', query: { __nextDefaultLocale: 'ru' } },
       '/about': { page: '/about' },
       '/blog': { page: '/blog' },
       '/services': { page: '/services' },
